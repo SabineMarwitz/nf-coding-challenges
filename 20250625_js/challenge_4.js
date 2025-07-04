ffc3403a793d9ac3e087f3fe8736e4aa7ad69102
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-07-04 20:20:35 +0200

    implemented challenge 4

--- 20250625_js/challenge_4.js
+++ 20250625_js/challenge_4.js
@@ -1,5 +1,33 @@
-function findMissingLetter(input) {
-    return "?";
-};
+// Find the missing letter
+// Example:
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
 
-console.log(findMissingLetter(findMissingLetter));
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const MyMap = new Map();
+
+for (let i = 0; i < alphabet.length; i++) {
+  MyMap.set(alphabet[i], i);
+}
+
+function findMissingLetter(inputString) {
+  const input = inputString.toUpperCase();
+  for (let i = 1; i < input.length; i++) {
+    const curLetter = input[i];
+    const lastLetter = input[i - 1];
+    const curLetterIdx = MyMap.get(curLetter);
+    const lastLetterIdx = MyMap.get(lastLetter);
+
+    const check = curLetterIdx - lastLetterIdx;
+    if (check > 1) {
+      // get the missing letter
+      const missingLetter = alphabet.charAt(lastLetterIdx + 1);
+      return missingLetter;
+    }
+  }
+  return "No missing letter found";
+}
+
+// Test
+console.log(findMissingLetter("ABCDF"));
+console.log(findMissingLetter("xz"));
